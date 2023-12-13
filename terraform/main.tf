0f97f7656322c5be286fb693b837ab0a4c126009
John Cheng <jcheng@natera.com>
2023-12-13 13:59:05 -0800

    sandbox2

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,3 +30,39 @@ module "sandbox1" {
 
 
 
+
+
+module "sandbox2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "lcheung+nsandbox2@natera.com"
+    AccountName               = "sandbox2"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "lcheung+nsandbox2@natera.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "Sandbox"
+  }
+
+  account_tags = {
+    "CreatedBy" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "jcheng"
+    change_reason       = "testing 1 2 3"
+  }
+
+  custom_fields = {
+    group = "nonprod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
+
+
+
+
+
+
