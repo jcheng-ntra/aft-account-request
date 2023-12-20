36557298706cd8a057ab3e18cb31fc806b39bd51
John Cheng <jcheng@natera.com>
2023-12-19 16:06:12 -0800

    removed sandbox1 and sandbox6

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,5 @@
 
 
-module "sandbox1" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "jcheng+nsandbox1@natera.com"
-    AccountName               = "sandbox1"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "jcheng+nsandbox1@natera.com"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "Sandbox"
-  }
-
-  account_tags = {
-    "CreatedBy" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "jcheng"
-    change_reason       = "testing 1 2 3"
-  }
-
-  custom_fields = {
-    group = "nonprod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
 
 
 
@@ -59,43 +31,6 @@ module "sandbox2" {
 
   account_customizations_name = "sandbox"
 }
-
-
-
-module "sandbox6" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "jcheng+nsandbox6@natera.com"
-    AccountName               = "sandbox6"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "jcheng+nsandbox6@natera.com"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "Sandbox"
-  }
-
-  account_tags = {
-    "CreatedBy" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "jcheng"
-    change_reason       = "testing 1 2 3"
-  }
-
-  custom_fields = {
-    group = "nonprod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
-
-
-
-
-
-
 
 
 
