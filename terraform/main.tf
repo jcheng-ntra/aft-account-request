7bd38bad2d928ec716f08231d7243d7b83167511
John Cheng <jcheng@natera.com>
2023-12-21 07:29:44 -0800

    sandbox8

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,4 +36,34 @@ module "sandbox2" {
 
 
 
+module "sandbox8" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jcheng+nsandbox8@natera.com"
+    AccountName               = "sandbox8"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "jcheng+nsandbox8@natera.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "Sandbox"
+  }
+
+  account_tags = {
+    "CreatedBy" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "jcheng"
+    change_reason       = "testing to confirm if limit has been raised based on comment from AWS Support 170298916501117"
+  }
+
+  custom_fields = {
+    group = "nonprod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
+
+
 
